f448d7b5548bcd0839ccfe006a064122d072f702
apiconz <picondev@gmail.com>
2017-05-12 19:29:50 -0500

    Clases y funciones

--- app/src/main/java/pe/devpicon/android/mykotlinandroidapp/MainActivity.kt
+++ app/src/main/java/pe/devpicon/android/mykotlinandroidapp/MainActivity.kt
@@ -2,11 +2,19 @@ package pe.devpicon.android.mykotlinandroidapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showHelloInSpanish("Hola! Mucho gusto!")
+    }
+
+    private fun showHelloInSpanish(message: String) {
+        val txtLabel = findViewById(R.id.txt_label) as TextView
+        txtLabel.text = message
     }
 }
